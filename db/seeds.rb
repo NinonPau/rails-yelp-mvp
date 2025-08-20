2d7a1818b158fa399591e8f44c3bdfb6b63fc161
Ninon Pau <ninon.pau@protonmail.com>
2025-08-20 15:34:07 +0200

    add the routes & add the restaurants controller and its views (#index, #show, #new) & add padding to the body

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 1. Clean the database
+puts "Cleaning the database..."
+Restaurant.destroy_all
+
+# 2. Creating the instances of the restaurant
+puts "Creating restaurants..."
+Restaurant.create!(name: "OTacos", address: "10 Rue du bon repas, Paris", category: "italian")
+puts "Created OTacos"
+Restaurant.create!(name: "MiamMiam!", address: "9 Av de la faim, Lyon", category: "belgian")
+puts "Created MiamMiam!"
+Restaurant.create!(name: "Good ol' Bar", address: "9 St Fish & Chips, London", category: "french")
+puts "Created Good ol' Bar"
+Restaurant.create!(name: "Itadakimasu", address: "2 A cool Japanese Street", category: "japanese")
+puts "Created Itadakimasu"
+Restaurant.create!(name: "Supa Good", address: "9 Chinatown", category: "chinese")
+puts "Created Supa Good"
+
+# 3. Display the finish message
+puts "Finished! Created #{Restaurant.count} restaurants."
